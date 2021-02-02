37561523214213412db55e03f524a0f94bcad348
Leon <leon.chow1@ontariotechu.net>
2021-02-02 16:21:49 -0500

    initialize main frontend components, migrate sass to scss, add angular material

--- frontend/AllChat/src/app/app-routing.module.ts
+++ frontend/AllChat/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChatComponent } from './chat/chat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', pathMatch: 'full', redirectTo: '', component: SignInComponent,
+  },
+  {
+    path: 'chat', pathMatch: 'full', component: ChatComponent,
+  },
+  {
+    path: '**', component: NotFoundComponent,
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/AllChat/src/app/app.module.ts
+++ frontend/AllChat/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { ChatComponent } from './chat/chat.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    ChatComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
