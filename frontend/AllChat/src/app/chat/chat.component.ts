0c5b27ed3d603d0da9b04d5c9ff3f1bde19a8b96
Leon <leon.chow1@ontariotechu.net>
2021-02-03 12:11:16 -0500

    Fix error with sample data structure,

--- frontend/AllChat/src/app/chat/chat.component.ts
+++ frontend/AllChat/src/app/chat/chat.component.ts
@@ -6,27 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit {
-  messages = {};
+  messages: string[][] = [];
 
   constructor() { }
 
   ngOnInit(): void {
     this.messages = [
-      {
-        "message": "Donec sit amet tincidunt nunc. Suspendisse et velit non arcu malesuada gravida sed id mauris. Sed at rutrum metus. Mauris et velit arcu. Fusce faucibus nisi id dolor vestibulum malesuada. In elit odio, fermentum non augue vel, mollis pulvinar lorem. Quisque semper, risus nec commodo pretium, lacus mauris pharetra metus, id pretium erat nunc quis lorem. Duis non libero in nunc dictum vehicula eget non risus. Suspendisse fringilla ut mi vitae semper. Donec ornare faucibus felis non varius. Curabitur convallis venenatis dictum. Suspendisse consectetur dignissim ligula, sed sollicitudin nibh luctus et. In hac habitasse platea dictumst. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla facilisi. Curabitur eget ipsum a mi fermentum aliquam.",
-        "user": "Jane Doe",
-        "avatar": "The Last Airbender"
-      },
-      {
-        "message": "Hello friend!",
-        "user": "John Smith",
-        "avatar": "Blue People",
-      },
-      {
-        "message": "Goodbye world!",
-        "user": "James Barov",
-        "avatar": "Legend of Korra"
-      }
-    ]
+      [
+        "Jane Doe",
+        "The Last Airbender",
+        "Donec sit amet tincidunt nunc. Suspendisse et velit non arcu malesuada gravida sed id mauris. Sed at rutrum metus. Mauris et velit arcu. Fusce faucibus nisi id dolor vestibulum malesuada. In elit odio, fermentum non augue vel, mollis pulvinar lorem. Quisque semper, risus nec commodo pretium, lacus mauris pharetra metus, id pretium erat nunc quis lorem. Duis non libero in nunc dictum vehicula eget non risus. Suspendisse fringilla ut mi vitae semper. Donec ornare faucibus felis non varius. Curabitur convallis venenatis dictum. Suspendisse consectetur dignissim ligula, sed sollicitudin nibh luctus et. In hac habitasse platea dictumst. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla facilisi. Curabitur eget ipsum a mi fermentum aliquam.",
+      ],
+      [
+        "John Smith",
+        "Blue People",
+        "Hello friend!",
+      ],
+      [
+        "James Barov",
+        "Legend of Korra",
+        "Goodbye world!",
+      ],
+    ];
   }
 }
