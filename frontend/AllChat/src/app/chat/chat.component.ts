6a8fc2f7343a0f833ec83b1d2745389e22b5da21
Leon <leon.chow1@ontariotechu.net>
2021-02-03 20:56:09 -0500

    Add sample channel changing function

--- frontend/AllChat/src/app/chat/chat.component.ts
+++ frontend/AllChat/src/app/chat/chat.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
   messages: string[][] = [];
+  channels: string[] = ['General', 'Games', 'Random', 'Work']
 
   constructor() { }
 
@@ -14,19 +15,8 @@ export class ChatComponent implements OnInit {
     this.messages = [
       [
         "Jane Doe",
-        "Sample",
         "Donec sit amet tincidunt nunc. Suspendisse et velit non arcu malesuada gravida sed id mauris. Sed at rutrum metus. Mauris et velit arcu. Fusce faucibus nisi id dolor vestibulum malesuada. In elit odio, fermentum non augue vel, mollis pulvinar lorem. Quisque semper, risus nec commodo pretium, lacus mauris pharetra metus, id pretium erat nunc quis lorem. Duis non libero in nunc dictum vehicula eget non risus. Suspendisse fringilla ut mi vitae semper. Donec ornare faucibus felis non varius. Curabitur convallis venenatis dictum. Suspendisse consectetur dignissim ligula, sed sollicitudin nibh luctus et. In hac habitasse platea dictumst. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla facilisi. Curabitur eget ipsum a mi fermentum aliquam aliquam. aliquam. aliquam aliquam. aliquam...",
-      ],
-      [
-        "John Smith",
-        "Sample",
-        "Hello friend!",
-      ],
-      [
-        "James Barov",
-        "Sample",
-        "Goodbye world!",
-      ],
+      ]
     ];
   }
 
@@ -34,8 +24,40 @@ export class ChatComponent implements OnInit {
   sendMessage(message: any): void {
     if (message.value.trim() != '' && message.value != '') {
       console.log(`posting message: ${message.value}`);
-      this.messages.push(["Bob Smith", "Sample", message.value]);
+      this.messages.push(["Bob Smith", message.value]);
       message.value = '';
+    }
+  }
+
+  changeChannels(channel: string) {
+    if (channel == "General") {
+      this.messages = [
+        [
+          "Bob Smith",
+          "Testing 123...",
+        ]
+      ];
+    } else if (channel == "Games") {
+      this.messages = [
+        [
+          "John Smith",
+          "Is this where I post my 360 no-scope videos?",
+        ]
+      ];
+    } else if (channel == "Random") {
+      this.messages = [
+        [
+          "Bob Bobless",
+          "Check out this awesome website at https://leonchow.me",
+        ]
+      ];
+    } else if (channel == "Work") {
+      this.messages = [
+        [
+          "James Barov",
+          "How do I make a REST API?",
+        ]
+      ];
     }
   }
 }
